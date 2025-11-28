30f0cc24f29dd7853e9917abd24506b2ad21d25a
grantlouisherman <grantlouisherman041@gmail.com>
2025-11-28 17:48:53 -0500

    basic constants

--- clox/chunk.c
+++ clox/chunk.c
@@ -7,8 +7,13 @@ void initChunk(Chunk* chunk){
   chunk->count = 0;
   chunk->capacity = 0;
   chunk->code = NULL;
+  initValueArray(&chunk->constants);
 }
 
+int addConstant(Chunk* chunk, Value value){
+  writeValueArray(&chunk->constants, value);
+  return chunk->constants.count - 1;
+}
 void writeChunk(Chunk* chunk, uint8_t byte){
   if(chunk->capacity < chunk->count + 1){
     int oldCapacity = chunk->capacity;
@@ -21,5 +26,6 @@ void writeChunk(Chunk* chunk, uint8_t byte){
 
 void freeChunk(Chunk* chunk){
   FREE_ARRAY(uint8_t, chunk->code, chunk->capacity);
+  freeValueArray(&chunk->constants);
   initChunk(chunk);
 }
