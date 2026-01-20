4fb76ea5e81a87b76e0fe18778db5d98c7147120
Grant Herman <gherma@meta.com>
2026-01-20 10:21:04 -0500

    c2

--- unix_utils/wcat.c
+++ unix_utils/wcat.c
@@ -4,6 +4,12 @@
 int main(int argc, char *argv[]){
   char file_contents;
   FILE *fp;
+  if(argc < 1){
+    printf("You need to pass in arguments");
+    return 0;
+  }
+  char file_path = *argv[1];
+  printf("%c", file_path);
   fp = fopen("/Users/gherma/Desktop/code_misc/c-practice/unix_utils/wcat.c", "r");
   if(fp == NULL){
     printf("File Error");
